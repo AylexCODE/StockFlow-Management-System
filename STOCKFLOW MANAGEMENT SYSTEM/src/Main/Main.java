842907428d7caf463c080eb539f38561eeb6ae92
Alex <130986014+AylexCODE@users.noreply.github.com>
2024-11-22 18:12:56 +0800

    Initial commit

---
+++ STOCKFLOW MANAGEMENT SYSTEM/src/Main/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Main;
+//import System.LogIn;
+/**
+ *
+ * @author crissa jean pagapong
+ */
+public class Main {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new MSystem.LogIn().setVisible(true);
+    }
+    
+    public static void run(){
+        new MSystem.LogIn().setVisible(true);
+    }
+}
